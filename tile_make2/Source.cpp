c886df1b30396796da338158b18815d839c26750
Kasugaccho <30593725+Kasugaccho@users.noreply.github.com>
2018-08-28 21:48:24 +0900

    tile loop

--- tile_make2/Source.cpp
+++ tile_make2/Source.cpp
@@ -3,24 +3,46 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	ChangeWindowMode(TRUE);
-	// ＤＸライブラリ初期化処理
-	if (DxLib_Init() == -1)
-	{
-		return -1;        // エラーが起きたら直ちに終了
-	}
+	SetGraphMode(1, 1, 32);
+	SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
+	if (DxLib_Init() == -1) return -1;
 
-	LoadGraph("a.jpg", TRUE);
 
-	while (CheckHitKeyAll() == 0)
-	{
-		// メッセージループに代わる処理をする
-		if (ProcessMessage() == -1)
-		{
-			break;        // エラーが起きたらループを抜ける
-		}
-	}
+	const int a = LoadGraph("1.png", TRUE);
+	int a_x{}, a_y{};
+	GetGraphSize(a, &a_x, &a_y);
+
+	const int screen = MakeScreen(a_x * 2, a_y, TRUE);
+	SetDrawScreen(screen);
+	ClearDrawScreen();
+
+	//描画
+	const int init_x = a_x / 2;
+	DrawExtendGraph(init_x + a_x + a_x, 0, init_x + a_x, a_y, a, TRUE);
+	DrawExtendGraph(init_x, 0, -init_x, a_y, a, TRUE);
+	DrawExtendGraph(init_x, 0, init_x + a_x, a_y, a, TRUE);
+
+	const int screen2 = MakeScreen(a_x * 2, a_y * 2, TRUE);
+	SetDrawScreen(screen2);
+	ClearDrawScreen();
+
+	//描画
+	const int init_y = a_y / 2;
+	DrawExtendGraph(0, init_y + a_y + a_y, a_x*2, init_y + a_y, screen, TRUE);
+	DrawExtendGraph(0, init_y, a_x*2, -init_y, screen, TRUE);
+	DrawExtendGraph(0, init_y, a_x*2, init_y + a_y, screen, TRUE);
+	DeleteGraph(screen);
+
+	const int screen3 = MakeScreen(a_x, a_y, TRUE);
+	SetDrawScreen(screen3);
+	ClearDrawScreen();
+
+	DrawExtendGraph(0, 0, a_x, a_y, screen2, TRUE);
+	DeleteGraph(screen2);
+
+	SaveDrawScreenToPNG(0, 0, a_x, a_y, "0.png", 0);
+	SaveDrawScreenToJPEG(0, 0, a_x, a_y, "0.jpg", 100, TRUE);
 
 	DxLib_End();
-
 	return 0;
 }
